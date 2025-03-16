31bb4e23ed6418772ae73f41f687d85141d4780a
Your Name <user@example.com>
2025-03-16 17:09:58 +0900

    feat: Partytownのインテグレーションを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://blog.butter-s.com",
-    integrations: [sitemap()],
+    integrations: [sitemap(), partytown()],
 })
